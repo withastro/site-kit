3d7a8955bdde6af7bf4f66047062f45886771547
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-11-29 15:08:58 -0600

    tweaking prettier and eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,33 @@
 module.exports = {
-    extends: ['plugin:astro/recommended', 'plugin:astro/jsx-a11y-recommended'],
+    extends: [
+      'plugin:@typescript-eslint/recommended',
+      'plugin:astro/recommended',
+      'plugin:astro/jsx-a11y-recommended',
+      'prettier',
+    ],
     overrides: [
-        {
-            // Define the configuration for `.astro` file.
-            files: ['*.astro'],
-            // Allows Astro components to be parsed.
-            parser: 'astro-eslint-parser',
-            // Parse the script in `.astro` as TypeScript by adding the following configuration.
-            // It's the setting you need when using TypeScript.
-            parserOptions: {
-                parser: '@typescript-eslint/parser',
-                extraFileExtensions: ['.astro']
-            },
-            rules: {
-                // override/add rules settings here, such as:
-                // "astro/no-set-html-directive": "error"
-            }
-        }
-        // ...
-    ]
-}
+      {
+        files: ['*.astro'],
+        parser: 'astro-eslint-parser',
+        parserOptions: {
+          parser: '@typescript-eslint/parser',
+          extraFileExtensions: ['.astro'],
+        },
+        rules: {},
+      },
+      {
+        files: ['*.ts'],
+        parser: '@typescript-eslint/parser',
+        parserOptions: {
+          ecmaVersion: 'latest',
+          sourceType: 'module',
+          rules: {
+            indent: ['error', 2],
+            'linebreak-style': ['error', 'unix'],
+            quotes: ['error', 'single'],
+            semi: ['error', 'never'],
+          },
+        },
+      },
+    ],
+  }
--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,14 +2,22 @@ module.exports = {
     plugins: [require.resolve('prettier-plugin-astro')],
     overrides: [
         {
+            files: ['.*', '*.json', '*.md', '*.toml', '*.yml'],
+            options: {
+                useTabs: false
+            }
+        },
+        {
             files: '*.astro',
             options: {
                 parser: 'astro'
             }
         }
     ],
-    trailingComma: 'none',
-    tabWidth: 4,
-    semi: false,
-    singleQuote: true
+    printWidth: 100,
+    semi: true,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: 'es5',
+    useTabs: true
 }
