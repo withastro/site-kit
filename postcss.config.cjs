0deabc1edc83d43aefb9e9b885884900982d71a2
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-12-12 10:54:41 -0600

    chore: :wastebasket: removing experimental components and tailwind config

--- postcss.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    plugins: {
-        tailwindcss: {
-            config: require('path').join(__dirname, 'tailwind.config.cjs') // update this if your path differs!
-        },
-        autoprefixer: {}
-    }
-}
