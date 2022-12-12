eeef419b840bc92c90e14f1cd4393c25f560dffe
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-12-12 16:06:56 -0600

    chore: :wastebasket: removing experimental components and tailwind config (#2)
    * chore: :wastebasket: removing experimental components and tailwind config
    
    * chore: :lock: fix pnpm lock file
    
    * disabling CI release step for now
    
    * Revert "disabling CI release step for now"
    
    This reverts commit 7de0a940466c097c32d15219c18c131574282f8a.
    
    * updating github actions to separate PR ci from releases
    
    * run linter in PR checks

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,33 +1,33 @@
 module.exports = {
-    extends: [
-      'plugin:@typescript-eslint/recommended',
-      'plugin:astro/recommended',
-      'plugin:astro/jsx-a11y-recommended',
-      'prettier',
-    ],
-    overrides: [
-      {
-        files: ['*.astro'],
-        parser: 'astro-eslint-parser',
-        parserOptions: {
-          parser: '@typescript-eslint/parser',
-          extraFileExtensions: ['.astro'],
-        },
-        rules: {},
-      },
-      {
-        files: ['*.ts'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:astro/recommended',
+    'plugin:astro/jsx-a11y-recommended',
+  ],
+  plugins: ['prettier'],
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
         parser: '@typescript-eslint/parser',
-        parserOptions: {
-          ecmaVersion: 'latest',
-          sourceType: 'module',
-          rules: {
-            indent: ['error', 2],
-            'linebreak-style': ['error', 'unix'],
-            quotes: ['error', 'single'],
-            semi: ['error', 'never'],
-          },
+        extraFileExtensions: ['.astro'],
+      },
+      rules: {},
+    },
+    {
+      files: ['*.ts'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        rules: {
+          indent: ['error', 2],
+          'linebreak-style': ['error', 'unix'],
+          quotes: ['error', 'single'],
+          semi: ['error', 'never'],
         },
       },
-    ],
-  }
+    },
+  ],
+};
