c451fb990742671a09a40cb716ac008235efdee6
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-12-12 16:04:30 -0600

    updating github actions to separate PR ci from releases

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,23 +1,23 @@
 module.exports = {
-    plugins: [require.resolve('prettier-plugin-astro')],
-    overrides: [
-        {
-            files: ['.*', '*.json', '*.md', '*.toml', '*.yml'],
-            options: {
-                useTabs: false
-            }
-        },
-        {
-            files: '*.astro',
-            options: {
-                parser: 'astro'
-            }
-        }
-    ],
-    printWidth: 100,
-    semi: true,
-    singleQuote: true,
-    tabWidth: 2,
-    trailingComma: 'es5',
-    useTabs: true
-}
+  plugins: [require.resolve('prettier-plugin-astro')],
+  overrides: [
+    {
+      files: ['.*', '*.json', '*.md', '*.toml', '*.yml'],
+      options: {
+        useTabs: false,
+      },
+    },
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: true,
+};
