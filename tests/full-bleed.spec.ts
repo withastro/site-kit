2dc00cd21310642c035027e097cdd160d37f853e
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-12-13 16:27:08 -0600

    linter formatting

--- tests/full-bleed.spec.ts
+++ tests/full-bleed.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test';
 
 test('<FullBleed />', async ({ page }) => {
-  await page.goto('/components/full-bleed');
+	await page.goto('/components/full-bleed');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/FullBleed | Astro/);
+	// Expect a title "to contain" a substring.
+	await expect(page).toHaveTitle(/FullBleed | Astro/);
 
-  expect(await page.screenshot()).toMatchSnapshot('components-full-bleed.png');
+	expect(await page.screenshot()).toMatchSnapshot('components-full-bleed.png');
 });
