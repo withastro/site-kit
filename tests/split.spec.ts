8449b84154fe440af1f0b4fa1ae0fe59d4256d2d
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-12-13 16:00:21 -0600

    feat: :sparkles: adds a <Split> component

---
+++ tests/split.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('<Split />', async ({ page }) => {
+  await page.goto('/components/split');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Split | Astro/);
+   
+  await page.setViewportSize({ width: 414, height: 966 });
+  expect(await page.screenshot()).toMatchSnapshot('components-split-mobile.png');
+
+  await page.setViewportSize({ width: 800, height: 1200 });
+  expect(await page.screenshot()).toMatchSnapshot('components-split-tablet.png');
+
+  await page.setViewportSize({ width: 1920, height: 1080 });
+  expect(await page.screenshot()).toMatchSnapshot('components-split-desktop.png');
+});
