9e99d364c347c8fbe3599ef2a32c7853f569d139
Tony Sullivan <tony.f.sullivan@outlook.com>
2023-03-27 15:37:15 -0500

    refactor: simplifying BaseHead into the SEO component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,7 @@
-import { default as BaseHead } from "./BaseHead.astro"
 import { default as ClickToggle } from "./ClickToggle.astro"
 import { default as SEO } from "./SEO.astro"
 
 export {
-	BaseHead,
 	ClickToggle,
 	SEO,
 }
