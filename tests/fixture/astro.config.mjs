64cf2a36684d469d070250d475036374755e3a2d
Tony Sullivan <tony.f.sullivan@outlook.com>
2023-04-03 14:39:30 -0500

    adding an extra sample test for the <SEO> component

--- tests/fixture/astro.config.mjs
+++ tests/fixture/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://test.com',
+	site: "https://test.com",
 	vite: {
 		ssr: {
 			noExternal: ["smartypants"],
